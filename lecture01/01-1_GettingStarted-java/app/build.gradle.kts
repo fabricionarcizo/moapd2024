bd95c5d6ea098f466440b6f87dfda6173d8750a1
Fabricio Batista Narcizo <fabricionarcizo@gmail.com>
2024-01-21 12:31:37 +0100

    Lecture #01 (Getting Started)
    This commit has the Android projects used during Lecture #01 of the Mobile App Development BSc. 2024 course **on January 30th, 2024**.

---
+++ lecture01/01-1_GettingStarted-java/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "dk.itu.moapd.gettingstarted"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dk.itu.moapd.gettingstarted"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
