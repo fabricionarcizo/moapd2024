08dcd963240498239a4ad24c9d97faf21b884baa
Fabricio Batista Narcizo <fabricionarcizo@gmail.com>
2024-01-28 16:34:12 +0100

    Lecture #02  (Introduction to Kotlin for Mobile Development)
    This commit has the Android projects used during Lecture #02 of the Mobile App Development BSc. 2024 course **on February 6th, 2024**.

---
+++ lecture02/02-1_Calculator/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "dk.itu.moapd.calculator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dk.itu.moapd.calculator"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.faendir.rhino:rhino-android:1.4")
+}
