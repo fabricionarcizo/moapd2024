5bddd08ff156235f940957025b2b078d83d25ff8
Fabricio Batista Narcizo <fabricionarcizo@gmail.com>
2024-02-04 12:20:55 +0100

    Lecture #03  (Android Framework UI Toolkit)
    This commit has the Android projects used during Lecture #03 of the Mobile App Development BSc. 2024 course **on February 13th, 2024**.

---
+++ lecture03/03-3_LifeCycle-vm/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "dk.itu.moapd.lifecycle"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dk.itu.moapd.lifecycle"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0")
+}
---
+++ lecture03/README.md
@@ -0,0 +1,9 @@
+# Lecture #03  (Android Framework UI Toolkit)
+
+This folder has the Android projects used during Lecture #03 of the Mobile App Development BSc. 2024 course **on February 13th, 2024**.
+
+[LifeCycle (Bug)](03-1_LifeCycle-bug): An Android app to print messages in the log system according to the activity life cycle. This project has a bug when the user changes the orientation of the Android device (i.e., portrait or landscape).
+
+[LifeCycle (Bundle)](03-2_LifeCycle-bundle): This project uses the method `onSaveInstanceState(Bundle)` to correct the bug when the user changes the orientation of the Android device (i.e., portrait or landscape).
+
+[LifeCycle (ViewModel)](03-3_LifeCycle-vm): This project uses a `ViewModel` to correct the bug when the user changes the orientation of the Android device (i.e., portrait or landscape).
